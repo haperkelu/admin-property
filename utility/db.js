4d67c81861c508c0159c71950b6adab5e176446e
paili <pai.li@lendi.com.au>
2017-07-27 21:01:51 +1000

    make change

--- utility/db.js
+++ utility/db.js
@@ -2,11 +2,12 @@
  * Created by LocalUser on 20/07/2017.
  */
 var mysql = require('mysql');
+var fs = require('fs');
 var cfg = JSON.parse(fs.readFileSync('./config/DB-Mysql.json', 'utf8'));
 var dbcon = mysql.createConnection(cfg);
 
 dbcon.connect(function(err) {
-    if (err) throw err;
+    if (err) console.log(err);
 });
 
 module.exports = dbcon;
