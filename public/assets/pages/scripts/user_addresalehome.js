ef49220da496d8c3159a1c4a0a0d7646e65a9319
Xiaoqian Yang <xiaoqian.yang.09@gmail.com>
2017-08-26 17:15:17 +1000

    add user_rent user_resalehome

--- public/assets/pages/scripts/user_addresalehome.js
+++ public/assets/pages/scripts/user_addresalehome.js
@@ -97,6 +97,13 @@ var Profile = function() {
                 }
             });
     }
+
+    var handleSummernote = function () {
+        $('#summernote_1').summernote({height: 300});
+        //API:
+        //var sHTML = $('#summernote_1').code(); // get code
+        //$('#summernote_1').destroy(); // destroy
+    }
     
     var handleAddress = function() {
 
@@ -110,6 +117,7 @@ var Profile = function() {
             //$("form").validate();
             handleTouchSpins();
             handleValidation();
+            handleSummernote();
         }
         
         
